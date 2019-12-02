e357a87d9123b388f074647de9faf52d5bd7355c
aearavindh <aearavindh@gmail.com>
2019-12-02 16:10:25 +0530

    MyCommit

--- src/test/java/guru/test99/AppTest.java
+++ src/test/java/guru/test99/AppTest.java
@@ -3,6 +3,7 @@ package guru.test99;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class AppTest {
@@ -12,27 +13,28 @@ public class AppTest {
   public void test() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver",
 		        "D:\\Eclipse\\test99\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-        driver.get("http://3.133.87.133:8080/BMI/");
-        driver.manage().window().maximize();
-        driver.manage().window().maximize();
+		ChromeOptions opt = new ChromeOptions();
+		opt.addArguments("--headless");
+		opt.addArguments("window-size=1920x1080");
+		opt.addArguments("--no-sandbox");
+		driver = new ChromeDriver(opt);
+		driver.get("http://3.134.101.222:8080/BMI/");
         Thread.sleep(1000);
-        driver.findElement(By.name("weight")).sendKeys("80.0");
-        Thread.sleep(1000);
-        driver.findElement(By.name("height")).sendKeys("2.0");
-        Thread.sleep(1000);
-        driver.findElement(By.cssSelector("input:nth-child(8)")).click();
-        Thread.sleep(2000);
-        String h= driver.getTitle();
-        String e= "BMI Calculator";
-        driver.close();
-        if(h.equalsIgnoreCase(e)){
-        System.out.println("Success");
-        }
-        else{
-        System.out.println("Failure");
-        }       
-        
+//		driver.findElement(By.name("weight")).sendKeys("80.0"); 
+//		Thread.sleep(1000);
+//		driver.findElement(By.name("height")).sendKeys("2.0"); 
+//		Thread.sleep(1000);
+//		driver.findElement(By.cssSelector("input:nth-child(8)")).click();
+//		Thread.sleep(2000);
+//        String h= driver.getTitle();
+//        System.out.println(h);
+//        String e= "BMI Calculator";
+//        if(h.equalsIgnoreCase(e)){
+//        System.out.println("Success");
+//        }
+//        else{
+//        System.out.println("Failure");
+//        }       
         driver.close();
 		
 	}
